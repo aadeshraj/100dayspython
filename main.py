433c06d7a68cd552dbe0d67a65ccc5e8e23f9047
Aadesh Raj Sharma <98164427+aadeshraj@users.noreply.github.com>
2025-03-20 17:11:04 +0000

    Created a program to generate Band's Name

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# Write your code below this line 👇
+print("Welcome to the Band Name Generator.")
+print("What's the name of the city you grew up in?")
+a = input()
+print("What is your pet's name?")
+b = input()
+d = a + " "
+e = b + " "
+f = d + e
+print("Your band's name could be " + f)
